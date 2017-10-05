9a5799c799341aeb19fd213e2c8275bffdab460f
G Walker <garretrwalker@gmail.com>
2017-10-05 12:28:02 -0400

    l

---
+++ src/org/cschool/gwalker/atm/AtmUI.java
@@ -0,0 +1,24 @@
+package org.cschool.gwalker.atm;
+
+import java.util.Scanner;
+
+public class AtmUI {
+	
+	private int UserID;
+	private int UserPin;
+
+	Scanner input = new Scanner(System.in);
+
+	public void getUserID(int UserID, int UserPin) {
+		this.display("Welcome to CCS6 Bank, Please Enter Your Personal ID:");
+		
+		this.UserID = 
+		
+
+	}
+
+	private void display(String message) {
+		System.out.println(message);
+	}
+
+}
